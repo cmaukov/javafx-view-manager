b445ba52fc1d7482167f1b91261114f5fd2599f4
konstantinstaykov <k.staykov@gmail.com>
2022-10-02 15:38:13 -0400

    added cache to reuse already loaded views. Without the cache, the view is loaded every time. In some situations, we need to load the view without using the cache.

--- src/main/java/com/bmstechpro/javafxviewmanager/ViewManager.java
+++ src/main/java/com/bmstechpro/javafxviewmanager/ViewManager.java
@@ -5,8 +5,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewManager {
+
+    private static Map<View, Parent> cache = new HashMap<>();
     private static Scene scene;
 
     public static void setScene(Scene scene) {
@@ -14,12 +18,18 @@ public class ViewManager {
     }
 
     public static void switchTo(View view) {
-        if(scene==null){
+        if (scene == null) {
             System.out.println("Scene was not set");
             return;
         }
         try {
-            Parent root = FXMLLoader.load(ViewManager.class.getResource(view.getFileName()));
+            Parent root;
+            if (cache.containsKey(view)) {
+                root = cache.get(view);
+            } else {
+                root = FXMLLoader.load(ViewManager.class.getResource(view.getFileName()));
+                cache.put(view, root);
+            }
             scene.setRoot(root);
         } catch (IOException e) {
             throw new RuntimeException(e);
